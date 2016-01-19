86bab8450f93cc28224eae309a6dbd71eb62784b
Jonathan Disla <jdisla2@mail.valenciacollege.edu>
2016-01-19 01:49:16 -0500

    Week 2 assignment
    Created language program

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,25 @@
-# replace the contents of this comment with your full name
-
-# write a program that:
+# Jonathan Disla
+    
 # 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
+
+print('Hello user!')
+print('Type the number 1, 2 or 3 to choose your spoken language.') 
+
+# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!)
+
+print('1. Spanish')
+print('2. English')
+print('3. French')
+
+lang = input()
+lang = int(lang)
+
 # 3. displays the greeting in the chosen language
-# 4. exits
+if lang == 1:
+    print('Hola mundo!')
 
-# make sure that your code contains comments explaining your logic!
+elif lang == 2:
+    print('Hello world!')
 
+elif lang == 3:
+    print('Salut monde!')
